60f30074660ee9c15eca1250f46a5aaa41c13394
Sniper-Progz <the.only.real.sniper@gmail.com>
2026-08-04 18:32:43 -0500

    Create __init__.py

---
+++ src/scout/shared_contracts/__init__.py
@@ -0,0 +1,18 @@
+"""Broker-neutral shared contracts for SCOUT."""
+
+from scout.shared_contracts.models import (
+    CandidateTicker,
+    MarketSnapshot,
+    StrategyRecommendation,
+    SystemHealth,
+    TradeSnapshot,
+)
+
+__all__ = [
+    "CandidateTicker",
+    "MarketSnapshot",
+    "StrategyRecommendation",
+    "SystemHealth",
+    "TradeSnapshot",
+]
+
